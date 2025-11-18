ade563db45464f031056bd5a936e59327ceb3604
swetha.k <swetha_kanneganti@yahoo.co.in>
2025-11-18 16:38:59 -0500

    async and file operations using Node.js

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,65 @@
 const fs = require('fs');
+const fsPromise = require('fs/promises');
 const path = require('path');
+const sampleTxt = path.join(__dirname, "sample-files", "sample.txt")
+const filePath1 = path.join(__dirname, "sample-files", "sample1.txt")
+const filePath2 = path.join(__dirname, "sample-files", "sample2.txt")
 
 
 // Write a sample file for demonstration
+if (!fs.existsSync(path.dirname(sampleTxt))) {
+  fs.mkdirSync(path.dirname(sampleTxt), { recursive: true });
+}
+fs.writeFileSync(sampleTxt, "Hello, async world!");
 
 // 1. Callback style
+
+ fs.readFile(sampleTxt, "utf-8", (err, data)=> {
+    if(err){
+      console.log(err);
+    } else{console.log("Callback read: ", data)}
+  })
 
 
   // Callback hell example (test and leave it in comments):
 
+   /*fs.readFile(sampleTxt, "utf-8", (err, data)=> {
+      if(err){
+        console.log(err);
+      } else{console.log("Callback read: ", data)}
+
+      fs.readFile(filePath1, "utf-8", (err, data1)=> {
+        if(err){
+          console.log(err);
+        } else{console.log("Callback read1: ", data1)}
+
+        fs.readFile(filePath2, "utf-8", (err, data2)=> {
+          if(err){
+            console.log(err);
+          } else{console.log("Callback read2: ", data2)}
+        })
+      })
+    })*/
+
 
   // 2. Promise style
-
+   
+   fsPromise.readFile(sampleTxt, "utf-8")
+      .then((data) => {
+        console.log("Promise read: ",data)
+      })
+      .catch((err) => {
+        console.log("Error reading file from promise:", err)
+      })
 
       // 3. Async/Await style
+
+      const readFileAsync = async () => {
+        try{
+          const asyncData = await fsPromise.readFile(sampleTxt, "utf-8")
+          console.log("Async/Await read: ",asyncData)
+        } catch(err){
+          console.log("Async/Await read error:", err)
+        }
+      }
+      readFileAsync();
--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -8,11 +8,25 @@ if (!fs.existsSync(sampleFilesDir)) {
 }
 
 // OS module
-
+console.log("Platform: ", os.platform());
+console.log("CPU: ",os.cpus());
+console.log("Total Memory: ", os.freemem());
 
 // Path module
+ 
+ console.log("Joined path: ", path.join('/assignment1','sample-files/sample.txt'))
 
 // fs.promises API
-
+  const {writeFile, readFile} = fs.promises
+  async function writeAndRead(){
+    try {
+      await writeFile('demo.txt', 'Hello from fs.promises!', 'utf-8');
+      const data = await readFile('demo.txt', 'utf-8')
+      console.log('fs.promises read: ',data)
+    } catch(err){
+      console.log(err)
+    }
+  }
+  writeAndRead();
 
 // Streams for large files- log first 40 chars of each chunk
